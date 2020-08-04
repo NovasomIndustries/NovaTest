cd7210b4bde8d3b7ccdcafae0f6dc472d1e4f393
mpnovasis <michele.puca@novasis.it>
2020-08-04 15:13:21 +0200

    remove everything

--- xampp/htdocs/b.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($_GET) {
-    $argument1 = $_GET['argument1'];
-}
-echo $argument1;
-$myfile = fopen("WebServerResult.txt", "w") or die("Unable to open file!");
-fwrite($myfile, $argument1);
-fclose($myfile);
-?>
